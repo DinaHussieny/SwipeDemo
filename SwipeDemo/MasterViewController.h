320f210b31f351d7fe59f918bc5f29588d200cb0
Dina Hussieny <dinahussieny@gmail.com>
2016-05-30 14:25:41 +0200

    Initial Commit

---
+++ SwipeDemo/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  SwipeDemo
+//
+//  Created by Dina Hussieny on 5/30/16.
+//  Copyright © 2016 Dina Hussieny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
---
+++ SwipeDemoTests/SwipeDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  SwipeDemoTests.m
+//  SwipeDemoTests
+//
+//  Created by Dina Hussieny on 5/30/16.
+//  Copyright © 2016 Dina Hussieny. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SwipeDemoTests : XCTestCase
+
+@end
+
+@implementation SwipeDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
